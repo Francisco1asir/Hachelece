ab0ac3201d16af8bbf9c431537c7351acad6fa5b
Francisco1asir <fparnav1908@g.educaand.es>
2023-11-29 10:59:41 +0000

    Prueba1

---
+++ web/index.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <?php echo "<h1>LF MANDA</h1>"?>
+    <marquee><?php echo "Server de Fraaaaaaaaaaan"?></marquee>
+</body>
+</html> 
---
+++ web/info/src/usuarios.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+    <?php echo "<h1>Listado de usuarios</h1>"?>
+</body>
+</html> 
